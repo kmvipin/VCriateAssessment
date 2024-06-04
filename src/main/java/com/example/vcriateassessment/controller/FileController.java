65b0e9ae771b51f4335cda53890f11109606c313
Vipin <vk783838@gmail.com>
2024-06-04 19:48:21 +0530

    Seperate file service and FileController

--- src/main/java/com/example/vcriateassessment/controller/FileController.java
+++ src/main/java/com/example/vcriateassessment/controller/FileController.java
@@ -2,31 +2,23 @@ package com.example.vcriateassessment.controller;
 
 import com.example.vcriateassessment.model.ApiResponse;
 import com.example.vcriateassessment.security.interf.MyUserDetails;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.vcriateassessment.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
-
-    @Value("${vcriate.file.location}")
-    private String path;
+    @Autowired
+    private FileService fileService;
 
     @PostMapping("/upload")
     public ResponseEntity<ApiResponse> uploadFile(@RequestParam("file") MultipartFile file, Principal principal) {
@@ -35,19 +27,12 @@ public class FileController {
         }
         String email = ((MyUserDetails)((UsernamePasswordAuthenticationToken) principal)
                 .getPrincipal()).getEmail();
-
-        Path fileLocation = Paths.get(this.path+File.separator+email);
-        File dir = new File(path+File.separator+email);
-        if(!dir.exists()){
-            dir.mkdir();
-        }
-        try {
-            Path targetLocation = fileLocation.resolve(file.getOriginalFilename());
-            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        boolean success = fileService.uploadFile(email,file);
+        if (success){
             ApiResponse apiResponse = new ApiResponse(true,"File Upload Successfully");
             return ResponseEntity.status(200).body(apiResponse);
-        } catch (Exception e) {
-            ApiResponse apiResponse = new ApiResponse(false,e.getMessage());
+        } else{
+            ApiResponse apiResponse = new ApiResponse(false,"Something Went Wrong");
             return ResponseEntity.status(500).body(apiResponse);
         }
     }
@@ -59,18 +44,10 @@ public class FileController {
         }
         String email = ((MyUserDetails)((UsernamePasswordAuthenticationToken) principal)
                 .getPrincipal()).getEmail();
-
-        Path fileLocation = Paths.get(this.path+File.separator+email);
-        try {
-            Path filePath = fileLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-
-            return ResponseEntity.ok()
+        Resource resource = fileService.downloadFile(email,fileName);
+        return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                     .body(resource);
-        } catch (Exception e) {
-            throw new RuntimeException("File download failed: " + e.getMessage());
-        }
     }
 
     @GetMapping("/info")
@@ -80,17 +57,7 @@ public class FileController {
         }
         String email = ((MyUserDetails)((UsernamePasswordAuthenticationToken) principal)
                 .getPrincipal()).getEmail();
-        String filesPath = path+File.separator+email;
-        Path filesLocation = Paths.get(filesPath);
-        List<String> filesName = new ArrayList<>();
-        try(Stream<Path> files = Files.list(filesLocation)){
-            files.forEach(filePath -> {
-                filesName.add(filePath.getFileName().toString());
-            });
-        }
-        catch (Exception e){
-            throw new RuntimeException("Path Does Not Exist");
-        }
+        List<String> filesName = fileService.getFileInfo(email);
         return ResponseEntity.status(200).body(filesName);
     }
 
@@ -101,17 +68,10 @@ public class FileController {
         }
         String email = ((MyUserDetails)((UsernamePasswordAuthenticationToken) principal)
                 .getPrincipal()).getEmail();
-
-        Path fileLocation = Paths.get(this.path+File.separator+email);
-        try {
-            Path filePath = fileLocation.resolve(filename).normalize();
-            Files.deleteIfExists(filePath);
-            ApiResponse apiResponse = new ApiResponse(true,"File Successfully Deleted");
-            return ResponseEntity.ok()
-                    .body(apiResponse);
-        } catch (Exception e) {
-            throw new RuntimeException("Something Went Wrong " + e.getMessage());
-        }
+        fileService.deleteFile(email,filename);
+        ApiResponse apiResponse = new ApiResponse(true,"File Successfully Deleted");
+        return ResponseEntity.ok()
+                .body(apiResponse);
     }
 
 }
--- src/main/java/com/example/vcriateassessment/service/FileService.java
+++ src/main/java/com/example/vcriateassessment/service/FileService.java
@@ -1,10 +1,80 @@
 package com.example.vcriateassessment.service;
 
+import com.example.vcriateassessment.model.ApiResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class FileService {
 
-    public
+    @Value("${vcriate.file.location}")
+    private String path;
 
+    public boolean uploadFile(String email, MultipartFile file){
+        Path fileLocation = Paths.get(this.path+ File.separator+email);
+        File dir = new File(this.path+File.separator+email);
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+        try {
+            Path targetLocation = fileLocation.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public Resource downloadFile(String email, String filename){
+        Path fileLocation = Paths.get(this.path+File.separator+email);
+        Path filePath = fileLocation.resolve(filename).normalize();
+        Resource resource;
+        try {
+            resource = new UrlResource(filePath.toUri());
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return resource;
+    }
+
+    public List<String> getFileInfo(String email){
+        String filesPath = this.path+File.separator+email;
+        Path filesLocation = Paths.get(filesPath);
+        List<String> filesName = new ArrayList<>();
+        try(Stream<Path> files = Files.list(filesLocation)){
+            files.forEach(filePath -> {
+                filesName.add(filePath.getFileName().toString());
+            });
+        }
+        catch (Exception e){
+            return new ArrayList<>();
+        }
+        return filesName;
+    }
+
+    public boolean deleteFile(String email, String filename){
+        Path fileLocation = Paths.get(this.path+File.separator+email);
+        try {
+            Path filePath = fileLocation.resolve(filename).normalize();
+            Files.deleteIfExists(filePath);
+        } catch (Exception e) {
+            throw new RuntimeException("Something Went Wrong " + e.getMessage());
+        }
+        return true;
+    }
 }
